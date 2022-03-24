4accfc002060ff255644e9fb0e9be5b35203c83c
Alexandre Costa <alexandreodpc@gmail.com>
2022-03-24 16:54:57 -0300

    escolha de filme

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -1,10 +1,17 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import Header from "../Header/";
+import TelaPricipal from "../TelaPricipal";
 
 function App() {
     return(
         <>
-            <Header />
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<TelaPricipal />} />
+                
+            </Routes>
+        </BrowserRouter>
+
         </>
     );
 }
---
+++ src/components/TelaPricipal/index.jsx
@@ -0,0 +1,47 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+
+import Header from "../Header";
+
+import "./style.css";
+
+export default function TelaPricipal() {
+    const [filmes, setFilmes] = useState([]);
+
+    useEffect(()=> {
+    const promise = axios.get("https://mock-api.driven.com.br/api/v5/cineflex/movies");
+    promise.then((resposta)=> {
+        const {data} = resposta;
+        setFilmes(data);
+    })
+    promise.catch((erro)=> {
+        console.log(erro);
+    })
+    }, []);
+
+    // filmes.map((filme) => {
+    //     console.log(filme.posterURL);
+    // })
+    
+    return(
+        <div className="TelaPricipal">
+            <Header />
+            <main>
+                <h2>Selecione o filme</h2>
+                <div className="listaFilmes">
+                {filmes.map((filme) => {
+                    return(
+                        <article className="filme" key={filme.id}>
+                            <Link to={`/sessoes/${filme.id}`}>
+                                <img src={filme.posterURL} alt={filme.title} />
+                            </Link>
+                        </article>
+                    );
+                })}
+                </div>
+                
+            </main>
+        </div>
+    );
+}
